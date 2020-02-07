f81c1162244cdced4144fd85ea89894b31f881c1
lakeofdance <58524964+lakeofdance@users.noreply.github.com>
2020-02-07 16:46:42 +0000

    Adding mon data
    base data and gfx, up to $1FD

---
+++ data/pokemon/base_stats+/mon_1D1.asm
@@ -0,0 +1,21 @@
+	dw MON_1D1 ; 000
+
+	db  10,  10,  10,  10,  10,  10
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, NORMAL ; type
+	db 250 ; catch rate
+	db 5 ; base exp
+	db NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 100 ; unknown 1
+	db 20 ; step cycles to hatch
+	db 5 ; unknown 2
+	INCBIN "gfx/pokemon/mon_1D1/front.dimensions"
+	db 0, 0, 0, 0 ; padding
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_HUMANSHAPE, EGG_HUMANSHAPE ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SNORE, PROTECT, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, REST, ATTRACT
+	; end
